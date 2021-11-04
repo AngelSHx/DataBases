f217563dd6487b51c0038536d6ac4bf5f6ab2ee2
snegi6 <negishivs@gmail.com>
2021-11-03 18:12:56 -0600

    added car and buyer tables

--- CarDealership/cuteCar_dealership.sql
+++ CarDealership/cuteCar_dealership.sql
@@ -20,14 +20,50 @@ CREATE TABLE IF NOT EXISTS `salespeople`(
 
     PRIMARY KEY (salesPersonId)
 );
-
+DROP TABLE IF EXISTS `car` ;
+CREATE TABLE IF NOT EXISTS `car` (
+	vinNumber VARCHAR(17) NOT NULL,
+    color VARCHAR(45) NOT NULL,
+    buyDate DATE NOT NULL,
+    sellDate DATE NOT NULL,
+    sellPrice DECIMAL NOT NULL,
+    salesPersonId INT NOT NULL,
+    sellerID INT NOT NULL,
+    buyerID INT NOT NULL,
+    
+    PRIMARY KEY (vinNumber)
+);
+DROP TABLE IF EXISTS `buyer` ;
+CREATE TABLE IF NOT EXISTS `buyer` (
+	buyerId INT NOT NULL,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    paymentMethod VARCHAR(45) NOT NULL,
+    
+    PRIMARY KEY (buyerId)
+);
 
 -- Load data using this: put your path to the files
-LOAD DATA Local INFILE "your path to the csv file"
+LOAD DATA Local INFILE "C:/Users/negis/Documents/DataBases/CarDealership/salespeople.csv"
 INTO TABLE salespeople 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA Local INFILE "C:/Users/negis/Documents/DataBases/CarDealership/buyer.csv"
+INTO TABLE buyer 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA Local INFILE "C:/Users/negis/Documents/DataBases/CarDealership/car.csv"
+INTO TABLE car 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- CHECK IF DATA IS IN TABLE
 select * from salespeople;
+select * from buyer;
+select * from car;
